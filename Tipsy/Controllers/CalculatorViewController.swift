d7ee6989e41d207dbc017b4fbf08c3545fad62f2
Dmytro Yantsybaiev <d.yantsybaiev@icloud.com>
2021-03-27 11:34:13 +0200

    Added setPctValue function which one set value according to chose percent.

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -9,6 +9,9 @@
 import UIKit
 
 class CalculatorViewController: UIViewController {
+    
+    var selectedButton: UIButton?
+    var pctValue: Float = -1.0
 
     @IBOutlet weak var billTextField: UITextField!
     @IBOutlet weak var zeroPctButton: UIButton!
@@ -16,24 +19,45 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UIView!
     
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setSelectedButton(pressedButton: tenPctButton)
+        setPctValue(pressedButton: tenPctButton)
+    }
+    
     @IBAction func tipChanged(_ sender: UIButton) {
-        setSelectedButton(button: sender.self)
+        setSelectedButton(pressedButton: sender.self)
+        setPctValue(pressedButton: sender.self)
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
+        print(pctValue)
     }
     
-    func setSelectedButton(button: UIButton) {
+    func setSelectedButton(pressedButton: UIButton) {
         let pctButtons = [zeroPctButton, tenPctButton, twentyPctButton]
         for pctButton in pctButtons {
-            if pctButton == button {
+            if pctButton == pressedButton {
+                selectedButton = pctButton
                 pctButton?.isSelected = true
             } else {
                 pctButton?.isSelected = false
             }
+        }
+    }
+    
+    func setPctValue(pressedButton: UIButton) {
+        if pressedButton == zeroPctButton {
+            pctValue = 0.0
+        } else if pressedButton == tenPctButton {
+            pctValue = 0.1
+        } else if pressedButton == twentyPctButton {
+            pctValue = 0.2
+        } else {
+            pctValue = -1.0
         }
     }
 
---
+++ Tipsy/Models/TipsyBrain.swift
@@ -0,0 +1,9 @@
+//
+//  TipsyBrain.swift
+//  Tipsy
+//
+//  Created by Dmytro Yantsybaiev on 27.03.2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import UIKit
