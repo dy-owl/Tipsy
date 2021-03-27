94d13f78b8e64e6b360a5f2f0bb2c4366b0b471d
Dmytro Yantsybaiev <d.yantsybaiev@icloud.com>
2021-03-27 12:33:43 +0200

    The stepper now change amount of people (splitNumberLabel)

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -17,7 +17,7 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var zeroPctButton: UIButton!
     @IBOutlet weak var tenPctButton: UIButton!
     @IBOutlet weak var twentyPctButton: UIButton!
-    @IBOutlet weak var splitNumberLabel: UIView!
+    @IBOutlet weak var splitNumberLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,6 +31,7 @@ class CalculatorViewController: UIViewController {
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
+        splitNumberLabel.text = String(Int(sender.value))
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
